b0abbf325d0344a0970f069a6ac9556d3d6cc932
Nick880804 <nick880804@gmail.com>
2023-01-12 10:19:34 +0800

    userproject &debug

--- Fundraising/wwwroot/MemberCentre/Js_src/UserSetting.js
+++ Fundraising/wwwroot/MemberCentre/Js_src/UserSetting.js
@@ -1,11 +1,33 @@
-let convertimage;
-let loadFile = function (event) {
-  let output = document.getElementById("output");
-  output.src = URL.createObjectURL(event.target.files[0]);
-  let reader = new FileReader();
-  output.onload = function () {
-    convertimage = reader.result;
-    URL.revokeObjectURL(output.src); // free memory
-  };
-  reader.readAsDataURL(event.target.files[0]);
+const app = {
+  data() {
+    return {
+      userinfo: {},
+    };
+  },
+  mounted() {
+    axios.get("/api/userinfo/setting/1").then((res) => {
+      this.userinfo = res.data;
+    });
+  },
+  methods: {
+    readURL(e) {
+      if (e.target.files && e.target.files[0]) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.onload = (e) => {
+          document.querySelector("#output").src = e.target.result;
+          this.userinfo.userPhoto = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    },
+    submitSetting() {
+      axios.put("/api/userinfo/setting/1", this.userinfo).then((res) => {
+        alert("儲存成功!");
+        history.go(0);
+      });
+    },
+  },
 };
+
+Vue.createApp(app).mount("#app");
