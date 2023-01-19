33ce2246b06416877cc0f7f87079375e85481644
Nick880804 <nick880804@gmail.com>
2023-01-19 15:07:57 +0800

    資料庫models更動

--- Fundraising/Models/User.cs
+++ Fundraising/Models/User.cs
@@ -10,10 +10,12 @@ namespace Fundraising.Models
         public User()
         {
             Answers = new HashSet<Answer>();
+            ChatroomUserId1Navigations = new HashSet<Chatroom>();
+            ChatroomUserId2Navigations = new HashSet<Chatroom>();
             Comments = new HashSet<Comment>();
             Followings = new HashSet<Following>();
-            MessageFromUsers = new HashSet<Message>();
-            MessageToUsers = new HashSet<Message>();
+            MessageReceivers = new HashSet<Message>();
+            MessageSenders = new HashSet<Message>();
             Orders = new HashSet<Order>();
         }
 
@@ -30,10 +32,12 @@ namespace Fundraising.Models
         public string UserPhoto { get; set; }
 
         public virtual ICollection<Answer> Answers { get; set; }
+        public virtual ICollection<Chatroom> ChatroomUserId1Navigations { get; set; }
+        public virtual ICollection<Chatroom> ChatroomUserId2Navigations { get; set; }
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<Following> Followings { get; set; }
-        public virtual ICollection<Message> MessageFromUsers { get; set; }
-        public virtual ICollection<Message> MessageToUsers { get; set; }
+        public virtual ICollection<Message> MessageReceivers { get; set; }
+        public virtual ICollection<Message> MessageSenders { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
     }
 }
